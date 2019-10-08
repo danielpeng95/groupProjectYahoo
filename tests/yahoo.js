a390b1147acccd509c12f1cb538016a3f23aadf7
danielpeng95 <danielpeng95@yahoo.com>
2019-10-08 12:13:19 -0600

    update

--- tests/yahoo.js
+++ tests/yahoo.js
@@ -32,38 +32,38 @@ module.exports = {
     after: browser => {
         Yahoo.end()
     },
-    // 'Can we Log in & Log out?': browser => { //Daniel
-    //     Yahoo
-    //         .click('@signIn')
-    //         .waitForElementVisible('@logIn')
-    //         .setValue('@logIn', ['softwareqa10@yahoo.com', browser.Keys.ENTER])
-    //         .waitForElementVisible('@pass')
-    //         .setValue('@pass', ['SoftQA1995', browser.Keys.ENTER])
-    //         .verify.containsText('@check', 'Software')
-    //         .api.pause(5000)
-    //     Yahoo
-    //         .waitForElementVisible('@ID')
-    //         .click('@ID')
-    //         // .moveToElement('@ID', 10, 10) //this will hover over //will work too
-    //         .waitForElementVisible('@out')
-    //         .click('@out')
-    // },
-    // 'Can we Search & Check Results?': browser => { //Nate
-    //     Yahoo
-    //         .waitForElementVisible('@searchBar')
-    //         .setValue('@searchBar', 'pizza')
-    //         .click('@searchButton')
-    //         .verify.containsText('[class=" reg searchCenterMiddle"]', 'pizza')
+    'Can we Log in & Log out?': browser => { //Daniel
+        Yahoo
+            .click('@signIn')
+            .waitForElementVisible('@logIn')
+            .setValue('@logIn', ['softwareqa10@yahoo.com', browser.Keys.ENTER])
+            .waitForElementVisible('@pass')
+            .setValue('@pass', ['SoftQA1995', browser.Keys.ENTER])
+            .verify.containsText('@check', 'Software')
+            .api.pause(5000)
+        Yahoo
+            .waitForElementVisible('@ID')
+            .click('@ID')
+            // .moveToElement('@ID', 10, 10) //this will hover over //will work too
+            .waitForElementVisible('@out')
+            .click('@out')
+    },
+    'Can we Search & Check Results?': browser => { //Nate
+        Yahoo
+            .waitForElementVisible('@searchBar')
+            .setValue('@searchBar', 'pizza')
+            .click('@searchButton')
+            .verify.containsText('[class=" reg searchCenterMiddle"]', 'pizza')
 
-    // },
-    // 'Can we change Tabs/Categories?': browser => { //Daniel
-    //     Yahoo
-    //     Categ(Yahoo) //function on top of this page
-    // },
-    // 'Do we get Daily news?': browser => { //Nate
-    //     Yahoo
+    },
+    'Can we change Tabs/Categories?': browser => { //Daniel
+        Yahoo
+        Categ(Yahoo) //function on top of this page
+    },
+    'Do we get Daily news?': browser => { //Nate
+        Yahoo
 
-    // },
+    },
     'Can we ask/post questions?': browser => { //Daniel 
         var originalWindow = ''
         var newWindow = ''
